25ad709ac06f8d7a720a34998c829fa158ee49f1
ELdiii <m4iky333111@gmail.com>
2022-10-06 17:30:52 +0200

    changed protocol

--- src/api/getChampions.js
+++ src/api/getChampions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ddragon.leagueoflegends.com/cdn/12.19.1/data/en_US/",
+  baseURL: "https://ddragon.leagueoflegends.com/cdn/12.19.1/data/en_US/",
 });
