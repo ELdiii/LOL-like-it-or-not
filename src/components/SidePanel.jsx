f46264117d05eae90fe6d4cf949b61fc08a83af6
ELdiii <m4iky333111@gmail.com>
2022-10-12 16:15:58 +0200

    SidePanel animation fix 2.0

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -3,15 +3,16 @@ import { AnimatePresence, motion } from "framer-motion";
 export default function SidePanel({ champArray, position }) {
   return (
     <>
-      <motion.div className="w-80">
+      <motion.div className="w-80 ">
         <AnimatePresence mode="popLayout">
           {champArray.map((champ) => (
             <motion.div
               layout
               key={champ.name}
-              initial={{ x: position === "left" ? -50 : 50, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ y: -30, opacity: 0 }}
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -18, opacity: 0 }}
+              transition={{ delay: 0.1 }}
               className=" text-white text-xl bg-gradient-to-r from-yellow-300 to-orange-500 bg-clip-text text-transparent "
             >
               {champ.name}
